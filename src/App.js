0c12c89bec9db05ecb61c7d9efa9ae2f8c1025af
Wei <wei@Weis-MacBook-Pro.local>
2018-10-19 00:03:53 +1100

    feat: set up firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyDj_-WpRWznkQA8oDuBndT6i6FYFxKW2_U",
+            authDomain: "auth-2b654.firebaseapp.com",
+            databaseURL: "https://auth-2b654.firebaseio.com",
+            projectId: "auth-2b654",
+            storageBucket: "auth-2b654.appspot.com",
+            messagingSenderId: "650993468331"
+        });
+    }
+
     render() {
         return (
             <View>
